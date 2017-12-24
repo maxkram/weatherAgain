162bf5162c9df2619176df6489b9df6699c87f6e
maxkram <max.kramer@live.com>
2017-12-24 13:32:29 -0600

    add storage and api keys

---
+++ weather.js
@@ -0,0 +1,16 @@
+class Weather {
+	constructor(city, state){
+		this.apiKey = "f1953769e4d9b46a";
+		this.city = city;
+		this.state = state;
+	}
+	async getWeather() {
+		const response = await fetch(`http://api.wunderground.com/api/${this.apiKey}/conditions/q/${this.state}/${this.city}.json`);
+		const responseData = await response.json();
+		return responseData.current_observation;
+	}
+	changeLocation(city, state){
+		this.city = city;
+		this.state = state;
+	}
+}
